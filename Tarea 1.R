4ee08cc1d29f4bccd7ca6f511dc4110ff875265d
Hysteric <44246648+Hysteric-Larva@users.noreply.github.com>
2024-04-05 00:31:25 -0300

    Add files via upload

--- Tarea 1.R
+++ Tarea 1.R
@@ -1,8 +1,11 @@
 library(GeoModels)
 library(mapproj)
 library(moments)
+library(spdep)
+library(sp)
 data(anomalies)
 head(anomalies,5)
+anomalies <- head(anomalies, 500)
 loc=cbind(anomalies[,1],anomalies[,2])
 z=cbind(anomalies[,3])
 summary(anomalies)
@@ -38,7 +41,7 @@ My <- function(lat,lon,z){
   # Iterate over the array using a for loop
   for (i in 1:n) {
     for (j in 1:n){
-      My_value <- My_value + sqrt((lat[i] - lat[j])^2 + (lon[i] - lon[j])^2)*(z[i]-z[j])
+      My_value <- My_value + sqrt((lat[i] - lat[j])^2 + (lon[i] - lon[j])^2)*abs(z[i]-z[j])
     }
   }
   return(My_value)
@@ -78,30 +81,57 @@ expected_value <- function(lat, lon,p) {
 Variance <- function(lat, lon,p) {
   norma <- 0
   n <- length(lat)  # Assuming lat and lon are vectors of the same length
-  
-  # Iterate over the array using a for loop
+    # Iterate over the array using a for loop
   for (i in 1:n) {
-    for (j in 1:n){
-      norma <- norma + sqrt((lat[i] - lat[j])^2 + (lon[i] - lon[j])^2)
+    for (j in 1:n) {
+      for (k in 1:n) {
+        for (l in 1:n) {
+          var_aux1<-0
+          norma_aux<-0
+          norma_aux <- sqrt((lat[i] - lat[j])^2 + (lon[i] - lon[j])^2)*sqrt((lat[k] - lat[l])^2 + (lon[k] - lon[l])^2)
+          if (i == j && k == l) {
+            next  # If i=j and k=l, continue to the next iteration
+          }
+          if (i == k || i == l || j == k || j == l) {
+            var_aux1 <- var_aux1 + (p * (1 - p) - 4 * p^2 * (1 - p))
+          }
+          if ((i == k && j == l) || (i == l && j == k)) {
+            var_aux1 <- var_aux1 + 4*p^2 * (1 - p^2)
+          }
+          if ((i != k && j == l) || (i != l && j == k) || (i == k && j != l) || (i == l && j != k)) {
+            var_aux1 <- var_aux1 + 4*p^3 * (1 - p)
+          }
+          norma = norma + norma_aux*var_aux1
+        }
+      }
     }
   }
   
-  variance_value <- norma * p * (1 - p) * 2*(1-2*p*(1-p))
+
+  
+  variance_value <- norma 
   return(variance_value)
 }  
   #############################################################
   
 get_myts<- function(lat,lon,z,p,tau){
   expected_value <- expected_value(lat, lon,p)
-  print(str(expected_value))
   variance_value <- Variance(lat,lon,p)
   print(str(variance_value))
   z_t <- Y_t(z,tau)
-  print(str(z_t))
   Myt <- My(lat,lon,z_t)
-  print(str(Myt))
   stMyt <- (Myt-expected_value)/sqrt(variance_value)
   return(stMyt)
   
 }
-get_myts(lat,lon,z,0.5,2)
+A = get_myts(lat,lon,z,0.5,2)
+#n=length(lat)
+
+#W <- knearneigh(anomalies, k = 100)  # k nearest neighbors
+#WT <- knn2nb(W)
+# Calculate Geary's C
+#geary_result <- geary.test(data$variable, listw = WT)
+
+# Print the result
+#print(geary_result)
+print(str(A))
